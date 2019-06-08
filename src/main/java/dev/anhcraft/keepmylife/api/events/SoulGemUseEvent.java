4744482ed485301195843d0359444feb82e35460
anhcraft <admin@anhcraft.dev>
2019-06-08 16:21:52 +0700

    update craftkit

---
+++ src/main/java/dev/anhcraft/keepmylife/api/events/SoulGemUseEvent.java
@@ -0,0 +1,42 @@
+package dev.anhcraft.keepmylife.api.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+public class SoulGemUseEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private Player player;
+    private boolean cancelled;
+
+    public SoulGemUseEvent(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
+    }
+}
